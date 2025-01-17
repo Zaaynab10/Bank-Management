f954ee0d056f8a85112571c780a37c50ec2c71fe
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2025-01-17 17:12:08 +0100

    Implement business logic to update transaction status to 'FAILED' when a transaction fails. Created a reusable method to handle failed transactions with dynamic types (Deposit, Withdrawal, Transfer).

--- src/Controller/TransactionsController.php
+++ src/Controller/TransactionsController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Controller;
+
+use App\Entity\BankAccount;
 use App\Entity\Transaction;
 use App\Enum\TransactionStatus;
 use App\Enum\TransactionType;
@@ -16,7 +18,34 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TransactionsController extends AbstractController
-{    #[Route('/transaction/deposit', name: 'app_deposit')]
+{   
+    
+    private function createFailedTransaction(
+        float $amount, 
+        BankAccount $sourceAccount, 
+        BankAccount $destinationAccount, 
+        TransactionType $transactionType,
+        EntityManagerInterface $entityManager
+    ) {
+        $transaction = new Transaction();
+    
+        $transaction->setAmount($amount);
+        $transaction->setDateTime(new \DateTime());
+        $transaction->setType($transactionType);
+        $transaction->setStatus(TransactionStatus::FAILED);
+        $transaction->setSourceAccount($sourceAccount);
+        $transaction->setDestinationAccount($destinationAccount);
+    
+        $entityManager->persist($transaction);
+        $entityManager->flush(); 
+    
+    }
+    
+    
+
+
+    
+    #[Route('/transaction/deposit', name: 'app_deposit')]
     public function MakeDeposit(
         Request $request, 
         EntityManagerInterface $entityManager, 
@@ -43,6 +72,8 @@ final class TransactionsController extends AbstractController
 
 
             if (!$bankAccount || $bankAccount->getOwner() !== $user) {
+                $this->createFailedTransaction($amount, $bankAccount, $bankAccount, TransactionType::DEPOSIT , $entityManager);
+
                 throw $this->createAccessDeniedException('You do not own this account.');
             }
 
@@ -110,6 +141,8 @@ final class TransactionsController extends AbstractController
 
             
             if (!$bankAccount->canWithdraw($amount)) {
+                $this->createFailedTransaction($amount, $bankAccount, $bankAccount, TransactionType::WITHDRAWAL, $entityManager);
+
                 throw $this->createAccessDeniedException('Insufficient funds.');
             }
             
@@ -163,21 +196,30 @@ final class TransactionsController extends AbstractController
             $amount = $form->get('amount')->getData();
     
             if ($sourceAccount === null) {
+
                 throw $this->createNotFoundException('Source account not found.');
             }
     
             $destinationAccount = $bankAccountRepository->findOneBy(['account_number' => $destinationAccountNumber]);
     
             if ($destinationAccount === null) {
+
                 throw $this->createNotFoundException('Destination account not found.');
             }
     
             if ($sourceAccount->getOwner() !== $user) {
+
                 throw $this->createAccessDeniedException('You do not own the source account.');
+
             }
     
             if (!$destinationAccount->canDeposit($amount)) {
+                $this->createFailedTransaction($amount, $sourceAccount, $destinationAccount, TransactionType::TRANSFER , $entityManager);
+
                 throw $this->createAccessDeniedException('Transfer denied: the savings account has exceeded its deposit limit of 25,000.');
+
+                $transaction->setStatus(TransactionStatus::FAILED);
+
             }
            
             
@@ -242,6 +284,8 @@ final class TransactionsController extends AbstractController
             'bankAccount' => $bankAccount,
         ]);
     }
+    
+
     
 
 }
