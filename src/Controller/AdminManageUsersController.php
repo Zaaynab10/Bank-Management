0a4399cea359a1e30fe921f48244b481ade993e1
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2025-01-21 00:53:12 +0100

    Manage toggle for activating and deactivating each bank account

--- src/Controller/AdminManageUsersController.php
+++ src/Controller/AdminManageUsersController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use App\Enum\BankAccountStatus;
 use App\Repository\BankAccountRepository;
+use App\Repository\UserRepository;
 use App\Service\AccountService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,17 +20,41 @@ class AdminManageUsersController extends AbstractController
         $this->accountService = $accountService;
     }
 
-    #[Route('/admin/user/{id}/accounts', name: 'admin_user_accounts')]
-    #[IsGranted('ROLE_ADMIN')] 
-    public function showUserAccounts(int $id): Response
-    {
-        $bankAccounts = $this->accountService->getUserAccounts($id);
 
-        return $this->render('admin/userAccounts.html.twig', [
-            'bankAccounts' => $bankAccounts,
-            'id' => $id,
-        ]);
-    }
+    #[Route('/admin/users', name: 'admin_users_list')]
+    public function listUsers(UserRepository $userRepository)
+    {
+        $users = $userRepository->findAll();
+        $userData = [];
+        foreach ($users as $user) {
+            $userData[] = [
+                'id' => $user->getId(),
+                'firstName' => $user->getFirstName(),
+                'lastName' => $user->getLastName(),
+                'email' => $user->getEmail(),
+            ];
+        }
+        return $this->render('admin/users.html.twig', [
+            'users'=>$userData
+        ]);    }
+        #[Route('/admin/user/{id}/accounts', name: 'admin_user_accounts')]
+        #[IsGranted('ROLE_ADMIN')] 
+        public function showUserAccounts(int $id, UserRepository $userRepository, BankAccountRepository $bankAccountRepository)
+        {
+            $user = $userRepository->find($id);
+        
+            if (!$user) {
+                throw $this->createNotFoundException("User not found.");
+            }
+        
+            $bankAccounts = $bankAccountRepository->findBy(['owner' => $user]);
+        
+            return $this->render('admin/userAccounts.html.twig', [
+                'user' => $user,            
+                'bankAccounts' => $bankAccounts,
+            ]);
+        }
+        
 
     #[Route('/admin/user/{id}/account/{accountId}/transactions', name: 'admin_account_transactions')]
     #[IsGranted('ROLE_ADMIN')] 
@@ -43,26 +68,25 @@ class AdminManageUsersController extends AbstractController
             'accountId' => $accountId,
         ]);
     }
-
-    #[Route('/admin/account/{id}/toggle-status', name: 'toggle_bank_account_status')]
-    #[IsGranted('ROLE_ADMIN')] 
-
-    public function toggleStatus(int $id, BankAccountRepository $bankAccountRepository, EntityManagerInterface $entityManager): Response
+    #[Route('/admin/user/{userId}/account/{accountId}/toggle-status', name: 'toggle_bank_account_status')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function toggleStatus(int $userId, int $accountId, BankAccountRepository $bankAccountRepository, EntityManagerInterface $entityManager): Response
     {
-        $bankAccount = $bankAccountRepository->find($id);
-        
+        $bankAccount = $bankAccountRepository->find($accountId);
+    
         if (!$bankAccount) {
             throw $this->createNotFoundException("Bank account not found.");
         }
-
+    
         if ($bankAccount->getStatus() === BankAccountStatus::ACTIVE) {
             $bankAccount->setStatus(BankAccountStatus::CLOSE);
         } else {
             $bankAccount->setStatus(BankAccountStatus::ACTIVE);
         }
-
+    
         $entityManager->flush();
-
-        return $this->redirectToRoute('admin_user_accounts');
+    
+        return $this->redirectToRoute('admin_user_accounts', ['id' => $userId]);
     }
+    
 }
